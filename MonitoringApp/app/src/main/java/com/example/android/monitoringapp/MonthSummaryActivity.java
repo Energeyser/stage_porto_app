b09c4e4374cf3de459539eacedf7d70236c6ee51
Unknown <annegaelle.servel@aliceadsl.fr>
2017-07-07 18:12:12 +0200

    Affichage d'un nouveau layout si un probleme est détecté

--- MonitoringApp/app/src/main/java/com/example/android/monitoringapp/MonthSummaryActivity.java
+++ MonitoringApp/app/src/main/java/com/example/android/monitoringapp/MonthSummaryActivity.java
@@ -4,19 +4,25 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import static com.example.android.monitoringapp.R.id.probleme_detected;
+
 public class MonthSummaryActivity extends AppCompatActivity {
+
+    boolean pbDetected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_month_summary);
 
-        /*
-        TextView putre =(TextView)findViewById(R.id.Rcin);
-        putre.setVisibility(View.VISIBLE); //show textview*/
-
+        if(pbDetected) {
+            LinearLayout pbView = (LinearLayout) findViewById(R.id.probleme_detected);
+            pbView.setVisibility(View.VISIBLE);
+        }
     }
 
     public void openMonthSummary(View view){
