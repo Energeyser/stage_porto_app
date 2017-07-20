df88e31d8bacb2cc038f5c1de556dc568596f4b9
Unknown <annegaelle.servel@aliceadsl.fr>
2017-07-20 12:58:32 +0200

    Identification pour modfier les inforamtions du docteur

--- MonitoringApp/app/src/main/java/com/example/android/monitoringapp/IdentificationActivity.java
+++ MonitoringApp/app/src/main/java/com/example/android/monitoringapp/IdentificationActivity.java
@@ -4,20 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.monitoringapp.Data.Doctor;
 import com.example.android.monitoringapp.Data.DoctorBDD;
 
+import static android.R.attr.password;
 
-public class DoctorInformationActivity extends AppCompatActivity {
+public class IdentificationActivity extends AppCompatActivity {
+
+    EditText login;
+    EditText password;
 
     TextView nameDoctor;
-    TextView phoneDoctor;
-    TextView emailDoctor;
     TextView nameDoctorInitials;
 
-    //New instance of the doctorBDD class
+    /**
+     * Database helper that will provide us access to the database
+     */
+    //New instance of the patientBDD class
     DoctorBDD doctorBDD = new DoctorBDD(this);
     //Creation of a patient
     Doctor doctor = new Doctor();
@@ -25,8 +32,7 @@ public class DoctorInformationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_doctor_information);
+        setContentView(R.layout.activity_identification);
 
         doctorBDD.open();
         doctor = doctorBDD.getDoctor();
@@ -34,14 +40,9 @@ public class DoctorInformationActivity extends AppCompatActivity {
 
         nameDoctor = (TextView) findViewById(R.id.name_doctor);
         nameDoctor.setText(doctor.getName());
-        phoneDoctor = (TextView) findViewById(R.id.phone_doctor);
-        phoneDoctor.setText(doctor.getPhone());
-        emailDoctor = (TextView) findViewById(R.id.mail_doctor);
-        emailDoctor.setText(doctor.getMail());
         nameDoctorInitials = (TextView) findViewById(R.id.name_doctor_initials);
         nameDoctorInitials.setText(doctor.getName().substring(0,1));
     }
-
 
     public void openMonthSummary(View view){
         Intent i = new Intent(this, MonthSummaryActivity.class);
@@ -63,9 +64,30 @@ public class DoctorInformationActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void openIdentificationActivity(View view){
-        Intent i = new Intent(this, IdentificationActivity.class);
-        startActivity(i);
+    public void onClickValidation(View v) {
+
+        //Get values enter in editText (login + password)
+        String log = "";
+        String pwd = "";
+
+        login = (EditText) findViewById(R.id.login_doctor);
+        password = (EditText) findViewById(R.id.password_doctor);
+        log = login.getText().toString().trim();
+        pwd = password.getText().toString().trim();
+
+        String user = doctor.getUsername().toString().trim();
+        String passwd = doctor.getPassword().toString().trim();
+
+        if (log.equals(user)) {
+            if(pwd.equals(passwd)){
+                Intent i = new Intent(this, DoctorModifyActivity.class);
+                startActivity(i);
+                Toast.makeText(getBaseContext(), "Identification OK", Toast.LENGTH_SHORT).show();
+            } else{
+                Toast.makeText(getBaseContext(), "Problem with your password", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(getBaseContext(), "Your identification is false, try again", Toast.LENGTH_LONG).show();
+        }
     }
-    
 }
